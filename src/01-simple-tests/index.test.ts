82290ab0c46285ce082d6c4735e42bc0cc1e8846
andreykomov <komov.te@gmail.com>
2024-10-18 00:22:57 +0200

    [task-tests] init commit

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,21 +1,45 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 10,
+      b: 10,
+      action: Action.Add,
+    };
+    const result = simpleCalculator(data);
+    expect(result).toBe(20);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 10,
+      b: 10,
+      action: Action.Subtract,
+    };
+    const result = simpleCalculator(data);
+    expect(result).toBe(0);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 10,
+      b: 10,
+      action: Action.Multiply,
+    };
+    const result = simpleCalculator(data);
+    expect(result).toBe(100);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 100,
+      b: 10,
+      action: Action.Divide,
+    };
+    const result = simpleCalculator(data);
+    expect(result).toBe(10);
   });
 
   test('should exponentiate two numbers', () => {
